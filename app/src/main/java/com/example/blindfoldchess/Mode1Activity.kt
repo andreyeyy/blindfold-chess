c8d8b354775bebe4e0779111400515e6e9940753
Andrey <116729572+andreyeyy@users.noreply.github.com>
2025-10-31 18:06:32 +0100

    Initial commit v1.0

---
+++ app/src/main/java/com/example/blindfoldchess/Mode1Activity.kt
@@ -0,0 +1,134 @@
+package com.example.blindfoldchess
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.blindfoldchess.ui.theme.BlindfoldChessTheme
+import kotlin.random.Random
+import android.os.Handler
+import android.os.Looper
+
+class Mode1Activity : ComponentActivity() {
+
+    private val darkSquares = arrayOf(
+        "a1", "a3", "a5", "a7", "b2", "b4", "b6", "b8",
+        "c1", "c3", "c5", "c7", "d2", "d4", "d6", "d8",
+        "e1", "e3", "e5", "e7", "f2", "f4", "f6", "f8",
+        "g1", "g3", "g5", "g7", "h2", "h4", "h6", "h8"
+    )
+
+    private val lightSquares = arrayOf(
+        "a2", "a4", "a6", "a8", "b1", "b3", "b5", "b7",
+        "c2", "c4", "c6", "c8", "d1", "d3", "d5", "d7",
+        "e2", "e4", "e6", "e8", "f1", "f3", "f5", "f7",
+        "g2", "g4", "g6", "g8", "h1", "h3", "h5", "h7"
+    )
+
+    private lateinit var tts: TTS
+    private val handler = Handler(Looper.getMainLooper())
+
+    private var isLightSquare = false
+
+    private var canClick = false
+
+    private val saySquareDelay = 1000L
+    private fun sayNextSquare(){
+        isLightSquare = Random.nextBoolean()
+
+        if (isLightSquare){
+            tts.say(lightSquares.random())
+        }
+        else {
+            tts.say(darkSquares.random())
+        }
+        canClick = true
+    }
+
+    private fun submitAnswer(isLight: Boolean){
+        if (canClick) {
+            canClick = false
+            if (isLight == isLightSquare) {
+                tts.say("Yes")
+            } else {
+                tts.say("No")
+            }
+            handler.postDelayed({ sayNextSquare() }, saySquareDelay)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        tts = (application as BlindfoldChessApp).tts
+
+        handler.postDelayed({ sayNextSquare() }, saySquareDelay)
+
+        setContent {
+            BlindfoldChessTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        SquareButton(
+                            text = "Light square",
+                            onClick = {
+                                submitAnswer(true)
+                            },
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxSize()
+                        )
+                        SquareButton(
+                            text = "Dark square",
+                            onClick = {
+                                submitAnswer(false)
+                            },
+                            modifier = Modifier
+                                .weight(1f)
+                                .fillMaxSize()
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SquareButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(2.dp, Color.Gray),
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Gray)
+    ) {
+        Text(text)
+    }
+}
---
+++ app/src/main/java/com/example/blindfoldchess/TTS.kt
@@ -0,0 +1,34 @@
+package com.example.blindfoldchess
+
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import java.util.Locale
+
+class TTS(context: Context) : TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech? = null
+
+    init {
+        tts = TextToSpeech(context, this)
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts?.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "The Language specified is not supported!")
+            }
+        } else {
+            Log.e("TTS", "Initialization Failed!")
+        }
+    }
+
+    fun say(text: String) {
+        tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    fun shutdown() {
+        tts?.stop()
+        tts?.shutdown()
+    }
+}
